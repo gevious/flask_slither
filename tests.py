4fa670cbd428ac8c4c5cc5353c125c0aba6efda8
Nico Gevers <nico@squire-labs.com>
2013-06-06 12:52:44 +0200

    Removed request signing authentication

--- tests.py
+++ tests.py
@@ -3,7 +3,6 @@ from contextlib import contextmanager
 from datetime import datetime, timedelta
 from flask import Flask, g
 from flask.ext.testing import TestCase
-from flask.ext.slither.authentication import RequestSigningAuthentication
 from flask.ext.slither.authorization import ReadOnlyAuthorization
 from flask.ext.slither.resources import BaseResource
 from flask.ext.slither.signals import request_authenticated, post_create
@@ -69,11 +68,6 @@ class Resource(BaseResource):
 class ReadOnlyAuthResource(BaseResource):
     collection = collection_name
     authorization = ReadOnlyAuthorization()
-
-
-class RequestSigningAuthenticationResource(BaseResource):
-    collection = collection_name
-    authentication = RequestSigningAuthentication()
 
 
 class BasicTestCase(TestCase):
@@ -396,117 +390,3 @@ class ReadOnlyAuthorizationTestCase(BasicTestCase):
         response = self.client.delete("/test/Record 4")
         self.assertEquals(response.status_code, 403)
         self.assertEquals(self.app.db[collection_name].count(), count)
-
-
-class RequestSigningAuthenticationTestCase(BasicTestCase):
-    def create_app(self):
-        app = Flask(__name__)
-        app.url_map.converters['regex'] = RegexConverter
-
-        app.config['DB_HOST'] = 'localhost'
-        app.config['DB_PORT'] = 27017
-        app.config['DB_NAME'] = 'test_slither'
-        app.config['TIMEZONE'] = 'Africa/Johannesburg'
-        app.client = MongoClient(app.config['DB_HOST'], app.config['DB_PORT'])
-        app.db = app.client[app.config['DB_NAME']]
-
-        # register test resource
-        register_api(app, RequestSigningAuthenticationResource, url="test")
-        return app
-
-    def headers(self, **kwargs):
-        # Generate header for a valid request to the squire api
-        url = "/localhost%s" % kwargs.get('url', "/test")
-        auth_keys = self.app.db['users'].find_one()['auth']
-        if '?' in url:
-            url = url.split('?')[0]
-        now = datetime.now(pytz.timezone(self.app.config['TIMEZONE'])) \
-            .replace(microsecond=0)
-        string_to_sign = (
-            u"%(verb)s\n%(content-type)s\n%(content-md5)s\n"
-            u"%(date)s\n%(resource)s") % {
-                'verb': kwargs.get('verb', "GET").lower(),
-                'content-type': kwargs.get('content-type', "application/json"),
-                'content-md5': kwargs.get('content-md5', ""),
-                'date': now, 'resource': url}
-        self.app.logger.debug("T: String to sign: %s" % string_to_sign)
-        a_key = str(kwargs.get('access_key', auth_keys['access_key']))
-        s_key = str(kwargs.get('secret_key', auth_keys['secret_key']))
-        hashed = hmac.new(s_key, string_to_sign, sha1)
-        signature = binascii.b2a_base64(hashed.digest())[:-1]
-
-        return {
-            'content_type': "application/json",
-            'headers': [('Authorization', "FS %s:%s" % (a_key, signature)),
-            ('fs-date', kwargs.get('date', now))]}
-
-    def test_no_authorization_header(self):
-        self.app.logger.info("T: Sending request without authorization")
-        response = self.client.get('/test')
-        self.assertEquals(response.status_code, 401)
-        self.assertEquals(response.json, "No authorization header")
-
-    def test_no_FS(self):
-        self.app.logger.info("T: Sending request without authorization")
-        headers = [('Authorization', "test")]
-        response = self.client.get('/test', headers=headers)
-        self.assertEquals(response.status_code, 401)
-        self.assertEquals(response.json, "Malformed authorization header")
-
-    def test_no_date(self):
-        self.app.logger.info("T: Sending request without date")
-        headers = [('Authorization', "FS ")]
-        response = self.client.get('/test', headers=headers)
-        self.assertEquals(response.status_code, 401)
-        self.assertEquals(response.json, "Missing date in header")
-
-    def test_late_date(self):
-        self.app.logger.info("T: Sending request with late date")
-        now = datetime.now(pytz.timezone(self.app.config['TIMEZONE'])) \
-            .replace(microsecond=0)
-        late = now - timedelta(minutes=15, seconds=1)
-
-        response = self.client.get('/test', **self.headers(date=late))
-        self.assertEquals(response.status_code, 401)
-        self.assertEquals(response.json, "Date is outside auth window period")
-
-    def test_early_date(self):
-        self.app.logger.info("T: Sending request with early date")
-        now = datetime.now(pytz.timezone(self.app.config['TIMEZONE'])) \
-            .replace(microsecond=0)
-        early = now + timedelta(minutes=15, seconds=1)
-
-        response = self.client.get('/test', **self.headers(date=early))
-        self.assertEquals(response.status_code, 401)
-        self.assertEquals(response.json, "Date is outside auth window period")
-
-    def test_bad_access_key(self):
-        self.app.logger.info("T: Sending request with bad access key")
-        response = self.client.get('/test', **self.headers(access_key='asdf'))
-        self.assertEquals(response.status_code, 401)
-        self.assertEquals(response.json, "Access denied")
-
-    def test_invalid_signature(self):
-        self.app.logger.info("T: Sending request with bad signature")
-        now = datetime.now(pytz.timezone(self.app.config['TIMEZONE'])) \
-            .replace(microsecond=0)
-        header = [('Authorization', "FS %s:%s" % ("access", "secret")),
-                  ('Date', now)]
-        response = self.client.get('/test', headers=header,
-                                   content_type="application/json")
-        self.assertEquals(response.status_code, 401)
-
-    def test_success(self):
-        self.app.logger.info("T: Sending successfully authenticated request")
-
-        with self.app.test_client() as c:
-            response = c.get('/test', **self.headers())
-            self.assertEquals(response.status_code, 200)
-            self.assertEquals(g.user['username'], 'testuser')
-
-    def test_success_signal(self):
-        self.app.logger.info("T: Testing signal for authenticated request")
-        with captured_auth_requests(self.app) as arqs:
-            self.app.test_client().get('/test', **self.headers())
-            self.assertEquals(len(arqs), 1)
-            self.assertEquals(arqs[0]['username'], "testuser")
