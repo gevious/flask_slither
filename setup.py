24adaf6b6c8e62e0c2a498461ec965ab07e2ef6d
Nico Gevers <nico@squire-labs.com>
2013-09-27 13:01:54 +0200

    Version 0.4.4
    Fixed #15 and #16

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Slither',
-    version='0.4.3',
+    version='0.4.4',
     url='http://github.com/gevious/flask_slither',
     license='MIT',
     author='Nico Gevers',
