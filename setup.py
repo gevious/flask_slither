86f4b5fcc9b4a2293fd7c0407836ca916569e664
Nico Gevers <nico@squire-labs.com>
2014-05-20 21:02:01 +0200

    Moved tests into a directory

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Slither',
-    version='0.4.18',
+    version='0.4.19',
     url='http://github.com/gevious/flask_slither',
     license='MIT',
     author='Nico Gevers',
