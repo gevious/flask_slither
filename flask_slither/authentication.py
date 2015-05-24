d910d79d86fa53590053952571a22687830e836b
Nico Gevers <nico@squire-labs.com>
2015-05-24 21:10:30 +0200

    removed old slither

--- flask_slither/authentication.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-
-
-class NoAuthentication():
-    """ The default authentication allows all requests through """
-    def is_authenticated(self, **kwargs):
-        current_app.logger.info("Authentication: No Auth checks in place")
-        return True
--- flask_slither/exceptions.py
+++
@@ -1,5 +0,0 @@
-class ApiException(Exception):
-    def __init__(self, message):
-        self.message = message
-        Exception.__init__(
-            self, "Message not found exception: missing {}".format(message))
